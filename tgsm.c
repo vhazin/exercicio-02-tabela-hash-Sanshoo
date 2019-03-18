ce1c07e1dcddec0d4d80778ea74161fbfadab2c2
Sanshoo <tomasgsmanzi@gmail.com>
2019-03-17 21:45:40 -0300

    bugfix

--- tgsm.c
+++ tgsm.c
@@ -9,21 +9,21 @@ int main(){
   scanf("%i", &matrix[0][0]);
   for (int a = 0; a < matrix[0][0]; a++){
     scanf("%i %i", &matrix[current][0], &matrix[current][1]);
-    for (a = 0; a < matrix[current][1]; a++){
-      scanf("%i", &matrix[current+1][a]);
+    for (int i = 0; i < matrix[current][1]; i++){
+      scanf("%i", &matrix[current+1][i]);
     }
     current = current+2;
   }
 
   current = 1;
   for (int a = 0; a < matrix[0][0]; a++){
-    for (a = 0; a < matrix[current][0]; a++){
-      printf("%i -> ", a);
-      for (a = 0; a < matrix[current][1]; a++){
-        if (((matrix[current+1][a]) % (matrix[current][0])) == (a)){
-          printf("%i -> ", matrix[current+1][a]); 
+    for (int b = 0; b < matrix[current][0]; b++){
+      printf("%i -> ", b);
+      for (int i = 0; i < matrix[current][1]; i++){
+        if (((matrix[current+1][i]) % (matrix[current][0])) == (b)){
+          printf("%i -> ", matrix[current+1][i]); 
         }
-      printf("\\ \n");
+      printf("\\\n");
       }
     }
     current = current+2;
